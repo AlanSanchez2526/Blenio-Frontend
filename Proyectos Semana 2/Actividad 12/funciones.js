097fe5eb326db1b85cf10ebbcba7255d5c332453
AlanSanchezR <alansanchezr2526@gmail.com>
2021-01-21 22:32:26 -0600

    Actividad 11

---
+++ Proyectos Semana 2/Actividad 12/funciones.js
@@ -0,0 +1,9 @@
+function comparar(){
+  const val1 = Number(prompt("20"));
+  const val2 = Number (prompt("30"));
+  if(val1>val2){
+    alert (val1 + "Es Mayor Que" + val2);
+  }else{
+    alert (val2 + "Es Mayor Que" + val1);
+  }
+}
