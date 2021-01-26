03c53a75a84cc48bf1cdb96da5c2ec6f1c07a3c0
AlanSanchezR <alansanchezr2526@gmail.com>
2021-01-26 01:35:32 -0600

    Proyecto 2

---
+++ Proyectos Semana 2/Proyecto 2/votacion.js
@@ -0,0 +1,9 @@
+let contador = 3;
+function masUno ( ){
+  contador = contador + 1;
+  document.getElementById("votos").innerHTML = contador + "votos";
+}
+function menosUno ( ){
+  contador = contador - 1;
+  document.getElementById("votos").innerHTML = contador + "votos";
+}
