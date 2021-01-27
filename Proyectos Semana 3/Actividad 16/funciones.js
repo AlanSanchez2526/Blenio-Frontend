9711050252654ad972ff56a9498b4a328c0a72ee
AlanSanchezR <alansanchezr2526@gmail.com>
2021-01-26 21:55:48 -0600

    Actividad 16

--- Proyectos Semana 3/Actividad 16/funciones.js
+++ Proyectos Semana 3/Actividad 16/funciones.js
@@ -1,4 +1,4 @@
-function Usuario{
+function Usuario (nombre, correo, edad, comentario){
   this.nombre = nombre;
   this.correo = correo;
   this.edad = edad;
@@ -9,6 +9,13 @@ const usuario1 = new Usuario("Anahid","anahid.correo.com",30,"diseñadora");
 const usuario2 = new Usuario("Roman","roman.correo.com",22,"Desarrollador en Progreso");
 const usuario3 = new Usuario("Alan","alan.correo.com",29,"Encargado de Region");
 
-const datos [usuario1,usuario2,usuario3]
+const datos = [usuario1,usuario2,usuario3]
 
 console.table(datos)
+
+datos.forEach(dato=>{
+        console.log(`********** Datos de Usuario ${dato.nombre} **********`)
+        for(propiedad in dato){
+             console.log(dato[propiedad])
+        }
+   })
