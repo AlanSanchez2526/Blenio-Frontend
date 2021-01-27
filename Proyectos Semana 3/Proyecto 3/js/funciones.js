447a024b87ffb447760aa43ed235ef24836d3ad9
AlanSanchezR <alansanchezr2526@gmail.com>
2021-01-26 19:51:17 -0600

    Proyecto 3

---
+++ Proyectos Semana 3/Proyecto 3/js/funciones.js
@@ -0,0 +1,9 @@
+function abrirPanel{
+ const paneles = document.querySelectorAll(".panel")
+ paneles.forEach(panel => panel.classList.remove("activa")); 
+
+ const opciones = document.querySelectorAll(".opcion")
+ opciones.forEach(opcion => opcion.classList.remove("activo"))
+
+ document.getElementById(panelInicial).classList.add("activa")
+}
